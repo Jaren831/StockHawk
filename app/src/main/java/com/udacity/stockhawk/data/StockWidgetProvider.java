4d5445e0ed384b1d83c9d255e3274c67833c7dcc
Jaren Lynch <Jaren Lynch>
2016-12-14 23:42:30 -0800

    Adds udacity course widget parts

--- app/src/main/java/com/udacity/stockhawk/data/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/data/StockWidgetProvider.java
@@ -1,14 +1,13 @@
 package com.udacity.stockhawk.data;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.RemoteViews;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 
-import com.udacity.stockhawk.R;
-import com.udacity.stockhawk.ui.MainActivity;
+import com.udacity.stockhawk.sync.QuoteSyncJob;
 
 /**
  * Created by jaren on 12/14/2016.
@@ -16,23 +15,22 @@ import com.udacity.stockhawk.ui.MainActivity;
 
 public class StockWidgetProvider extends AppWidgetProvider {
 
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        final int N = appWidgetIds.length;
+        context.startService(new Intent(context, StockWidgetService.class));
+    }
 
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int i=0; i<N; i++) {
-            int appWidgetId = appWidgetIds[i];
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
+                                          int appWidgetId, Bundle newOptions) {
+        context.startService(new Intent(context, StockWidgetService.class));
+    }
 
-            // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_layout);
-
-            // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+        if (QuoteSyncJob.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            context.startService(new Intent(context, StockWidgetService.class));
         }
     }
 }
---
+++ app/src/main/java/com/udacity/stockhawk/data/StockWidgetService.java
@@ -0,0 +1,8 @@
+package com.udacity.stockhawk.data;
+
+/**
+ * Created by jaren on 12/14/2016.
+ */
+
+public class StockWidgetService {
+}
